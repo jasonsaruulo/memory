997bf4377a459acba1420fdaedd40f30b192dbae
Saruul Shafiq <saruul.s@googlemail.com>
2017-12-10 23:22:07 +0100

    Incremented min sdk version to 16. Added ButterKnife dependency. Asking for read external storage permission when new game button is clicked.

--- app/src/main/java/com/saruul/shafiq/memory/MainActivity.kt
+++ app/src/main/java/com/saruul/shafiq/memory/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.saruul.shafiq.memory
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
+
+    private val readExternalStoragePermissionRequest = 100
+
+    @BindView(R.id.main_permission_explanation) lateinit var permissionExplanation: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+    }
+
+    @OnClick(R.id.main_new_game)
+    fun requestReadExternalStoragePermission() {
+        val permissionReadExternalStorage = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (permissionReadExternalStorage != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                permissionExplanation.setText(
+                        R.string.main_read_external_storage_permission_explanation)
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        readExternalStoragePermissionRequest)
+            }
+        } else {
+            // TODO: Load images
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isEmpty()) {
+            return
+        }
+        when (requestCode) {
+            readExternalStoragePermissionRequest ->
+                if (grantResults.first() == PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Load images
+                } else {
+                    permissionExplanation.setText(
+                            R.string.main_read_external_storage_permission_explanation)
+                }
+        }
     }
 }
